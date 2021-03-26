33a98a781d0112bbb6a8e9f365a1fcd2c9337712
ParkDahye <clo_udhye@naver.com>
2021-03-26 11:13:49 +0900

    사이드바 수정

--- src/main/webapp/js/sidebar.js
+++ src/main/webapp/js/sidebar.js
@@ -1,10 +1,10 @@
 function sidebarCollapse() {
    if(document.getElementById("mySidebar").style.width == "250px"){
       document.getElementById("mySidebar").style.width = "0";
-        document.getElementById("main").style.marginLeft = "0";
+        document.getElementById("header").style.marginLeft = "0";
    } 
    else {
       document.getElementById("mySidebar").style.width = "250px";
-      document.getElementById("main").style.marginLeft = "250px";
+      document.getElementById("header").style.marginLeft = "250px";
    }
 }
